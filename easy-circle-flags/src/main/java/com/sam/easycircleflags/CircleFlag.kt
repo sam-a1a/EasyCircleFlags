1539d7b16dacec6e1debb4297b3b19ea2967c89b
Sam <BassamGhazaleh@proton.me>
2026-05-17 23:05:58 +0300

    Fix SVG decoder

--- easy-circle-flags/src/main/java/com/sam/easycircleflags/CircleFlag.kt
+++ easy-circle-flags/src/main/java/com/sam/easycircleflags/CircleFlag.kt
@@ -1,7 +1,9 @@
+// src/main/java/com/sam/easycircleflags/CircleFlag.kt
 package com.sam.easycircleflags
 
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
@@ -11,10 +13,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil3.ImageLoader
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
 import coil3.size.Size
+import coil3.svg.SvgDecoder
 
 @Composable
 fun CircleFlag(
@@ -44,12 +48,21 @@ fun CircleFlag(
         .crossfade(true)
         .build()
 
+    val svgImageLoader = remember {
+        ImageLoader.Builder(context.applicationContext)
+            .components {
+                add(SvgDecoder.Factory())
+            }
+            .build()
+    }
+
     AsyncImage(
         model = imageRequest,
         contentDescription = contentDescription,
         modifier = modifier.size(size),
         contentScale = contentScale,
         placeholder = placeholder,
-        error = error
+        error = error,
+        imageLoader = svgImageLoader
     )
 }
